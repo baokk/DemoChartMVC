7d0c7c9f6d7f518b34d4d271bd72f6b216db5e64
lecongphuc92 <lecongphuc92@gmail.com>
2016-04-05 00:16:45 +0700

    Add project

---
+++ DemoChart/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoChart.Controllers
+{
+    public class DemoController : Controller
+    {
+        List<int> lstOrderHY = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        List<int> lstOrderHN = new List<int> { 3, 2, 5, 4, 5, 4, 7, 8, 9, 3, 7, 9 };
+        //
+        // GET: /Demo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LoadOrder(string location)
+        {
+            if (location == "HY")
+            {
+                return Json(new { Items = lstOrderHY.ToList() }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { Items = lstOrderHN.ToList() }, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+	}
+}
---
+++ DemoChart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoChart.Startup))]
+namespace DemoChart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
